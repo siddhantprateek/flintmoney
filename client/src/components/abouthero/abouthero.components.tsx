7f963490a46ee17ebaa316e4339a5b7c933a12d1
Siddhant Prateek <siddhantprateek@gmail.com>
2022-08-19 22:56:58 +0530

    about hero component

---
+++ client/src/components/abouthero/abouthero.components.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// styles
+import './abouthero.styles.css'
+
+const Abouthero = () => {
+  return (
+    <div className='about-hero-container'>
+        <h1>We are building <br/> you a home</h1>
+        <p>in the world of crypto</p>
+    </div>
+  )
+}
+
+export default Abouthero;
